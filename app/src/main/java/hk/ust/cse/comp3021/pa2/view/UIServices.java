8a061f99b15a93de5578ce7a2ecb46711e5c123f
Che Siu Hei <chesiuhei1322@gmail.com>
2021-11-04 22:45:53 +0800

    UIServices implemented

--- app/src/main/java/hk/ust/cse/comp3021/pa2/view/UIServices.java
+++ app/src/main/java/hk/ust/cse/comp3021/pa2/view/UIServices.java
@@ -4,11 +4,16 @@ import hk.ust.cse.comp3021.pa2.InertiaFxGame;
 import hk.ust.cse.comp3021.pa2.model.GameState;
 import hk.ust.cse.comp3021.pa2.util.GameStateSerializer;
 import hk.ust.cse.comp3021.pa2.util.NotImplementedException;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -26,9 +31,12 @@ public class UIServices {
      */
     @NotNull
     public static FileChooser createGameLoadFileChooser() {
-        // TODO: Create a file chooser with the provided string above \
+        // TODO ok: Create a file chooser with the provided string above \
         //  and set its initial directory to be the current working directory.
-        throw new NotImplementedException();
+        FileChooser x = new FileChooser();
+        x.setTitle(FILE_CHOOSER_TITLE);
+        x.setInitialDirectory(new File(String.valueOf(getWorkingDirectory())));
+        return x;
     }
 
     /**
@@ -74,8 +82,13 @@ public class UIServices {
      * Shows an {@link Alert} telling that there is an error when trying to open the game file.
      */
     public static void showLoadGameErrorDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // TODO: ok Show a dialog using the provided strings above.
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(LOAD_GAME_ERROR_ALERT_TITLE);
+        alert.setHeaderText("Message");
+        alert.setContentText(LOAD_GAME_ERROR_ALERT_CONTENT_TEXT);
+
+        alert.showAndWait();
     }
 
     private static final String LOAD_GAME_CANCEL_ALERT_TITLE = "Operation Cancelled";
@@ -86,8 +99,13 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has cancelled loading a game file.
      */
     public static void showFileNotSelectedDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // TODO: ok Show a dialog using the provided strings above.
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(LOAD_GAME_CANCEL_ALERT_TITLE);
+        alert.setHeaderText("Message");
+        alert.setContentText(LOAD_GAME_CANCEL_ALERT_CONTENT_TEXT);
+
+        alert.showAndWait();
     }
 
     private static final String WIN_ALERT_TITLE = "Congratulations";
@@ -98,8 +116,13 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has won the game.
      */
     public static void showWinDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // TODO: ok Show a dialog using the provided strings above.
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(WIN_ALERT_TITLE);
+        alert.setHeaderText("Message");
+        alert.setContentText(WIN_ALERT_CONTENT_TEXT);
+
+        alert.showAndWait();
     }
 
     private static final String LOSE_ALERT_TITLE = "Oops";
@@ -110,7 +133,12 @@ public class UIServices {
      * Shows an {@link Alert} telling that the user has lost the game.
      */
     public static void showLoseDialog() {
-        // TODO: Show a dialog using the provided strings above.
-        throw new NotImplementedException();
+        // TODO: ok Show a dialog using the provided strings above.
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(LOSE_ALERT_TITLE);
+        alert.setHeaderText("Message");
+        alert.setContentText(LOSE_ALERT_CONTENT_TEXT);
+
+        alert.showAndWait();
     }
 }

0ca82e1cd16d30e2f6b112764d49f3b5623f9c60
Che Siu Hei <chesiuhei1322@gmail.com>
2021-11-05 17:41:28 +0800

    GameBoardPane implemented

--- app/src/main/java/hk/ust/cse/comp3021/pa2/view/panes/GameBoardPane.java
+++ app/src/main/java/hk/ust/cse/comp3021/pa2/view/panes/GameBoardPane.java
@@ -1,10 +1,15 @@
 package hk.ust.cse.comp3021.pa2.view.panes;
 
+import hk.ust.cse.comp3021.pa2.model.GameBoard;
 import hk.ust.cse.comp3021.pa2.model.GameState;
 import hk.ust.cse.comp3021.pa2.util.NotImplementedException;
 import hk.ust.cse.comp3021.pa2.view.GameUIComponent;
+import hk.ust.cse.comp3021.pa2.view.controls.GameCell;
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A {@link javafx.scene.layout.Pane} for displaying the status of {@link hk.ust.cse.comp3021.pa2.model.GameBoard}.
@@ -26,8 +31,22 @@ public class GameBoardPane extends GridPane implements GameUIComponent {
      * @param gameState The latest {@link GameState}.
      */
     public void showGameState(GameState gameState) {
-        // TODO: Update the content based on the state of the game board.
-        throw new NotImplementedException();
+        // TODO: ok Update the content based on the state of the game board.
+        GridPane x = (GridPane) this.getChildren();
+        createGameBoardPane(gameState.getGameBoard().getNumRows(),
+                            gameState.getGameBoard().getNumCols(),
+                            x, gameState.getGameBoard());
+
+    }
+    //helper func
+    private static void createGameBoardPane(final int numRows, final int numCols, GridPane a, GameBoard x) {
+        a.setAlignment(Pos.CENTER);
+
+        for (int i = 0; i < numRows; i++){
+            for (int j = 0; j < numCols; j++){
+                a.add(new GameCell(x.getCell(i, j)),j,i);
+            }
+        }
     }
 
 }
